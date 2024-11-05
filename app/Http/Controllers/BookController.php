0c2d18118b174839a28673d0f0f340a9304436b9
MadyanArashy <madyan.arashy5@smk.belajar.id>
2024-11-05 08:32:10 +0700

    komissar

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,14 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Response;
 use Inertia\Inertia;
+use App\Models\Book;
 
 class BookController extends Controller
 {
-    public function index() {
+    public function index(): Response{
         return Inertia::render('books/index');
     }
-    public function create() {
+    public function create(): Response {
         return Inertia::render('books/create');
+    }
+    public function store(Request $request): RedirectResponse {
+        $request->validate([
+            'judul_buku' => 'string|required',
+            'pengarang' => 'string|required',
+            'penerbit' => 'string|required',
+            'deskripsi' => 'string|required',
+        ]);
+
+        Book::create($request->all());
+
+        return redirect()->route('books/index')->with('success','Anda telah menambahkan buku');
+    }
+    public function update(): RedirectResponse {
+        return redirect()->route('books/index')->with('success', 'Anda telah mengubah buku');
     }
 }
